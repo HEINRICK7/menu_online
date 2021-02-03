58104a819f17b67eb9fd52e42b613335cda8d658
Henrique <Carlos Henrique>
2021-02-03 14:44:01 -0300

    create: page src/components/Input

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const InputBase = styled.input`
+    //margin: 50px 0 30px 20px;
+    display: block;
+    margin-top: 50px;
+    margin-right: auto;
+    margin-left: auto;
+    width: 300px;
+    padding: 15px 10px 15px 20px;
+    background: #FFFFFF;
+    border: 1px solid #D9D0E3;
+    border-radius: 27px;
+`;
+
+const Input = ({ onChange, placeholder }) => {
+    return (
+        <>
+            <InputBase 
+            onChange={ onChange}
+            placeholder={ placeholder}
+             />
+        </>
+    );
+}
+
+//Input.propTypes = {
+//    onChange: PropTypes.func.isRequired,
+//    placeholder: PropTypes.string.isRequired,
+//};
+//
+export default Input;
