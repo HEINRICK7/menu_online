f303f7676f57565c9572f1a1087f50e69ae3d0b9
Henrique <Carlos Henrique>
2021-02-19 10:09:50 -0300

    modified: update layout

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+      <meta name="theme-color" content="#fcb045" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;800&display=swap" rel="stylesheet"/>
